58c1c6f372c7f2572494808e2876c3ddc3ce888d
Gustavo Rodrigues da Silva <gustavo.aprendiz09@gmail.com>
2026-04-05 14:41:40 -0300

    Create cinco_num.java

---
+++ cinco_num.java
@@ -0,0 +1,16 @@
+import static java.lang.IO.*;
+
+void main() {
+    println("Digite o 1º numero: ");
+    float num1 = Float.parseFloat(readln());
+    println("Digite o 2º numero: ");
+    float num2 = Float.parseFloat(readln());
+    println("Digite o 3º numero: ");
+    float num3 = Float.parseFloat(readln());
+    println("Digite o 4º numero: ");
+    float num4 = Float.parseFloat(readln());
+    println("Digite o 5º numero: ");
+    float num5 = Float.parseFloat(readln());
+
+    println("Seus numeros são: \n" + num1 + "\n" + num2 + "\n" + num3 + "\n" + num4 + "\n" + num5);
+}
